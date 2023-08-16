09b87f10920667465dd2aac393949108bac8e0a6
Andruxxa7 <andruxakosinec7@gmail.com>
2023-08-16 18:25:02 +0300

    Add intervals validation

--- CurrentlyPlayingInfoPlugin/Models/PluginSettings.cs
+++ CurrentlyPlayingInfoPlugin/Models/PluginSettings.cs
@@ -1,11 +1,22 @@
-﻿using System.Text.Json.Serialization;
-
-namespace AIMP.CurrentlyPlayingInfoPlugin.Models;
+﻿namespace AIMP.CurrentlyPlayingInfoPlugin.Models;
 
 public class PluginSettings
 {
-    public int Interval { get; set; } = 30;
-    public int WaitInterval { get; set; } = 60;
+    private int _interval = 30;
+    private int _waitInterval = 60;
+
+    public int Interval
+    {
+        get => _interval;
+        set => _interval = value is >= 10 and <= 300 ? value : 30;
+    }
+
+    public int WaitInterval
+    {
+        get => _waitInterval;
+        set => _waitInterval = value is >= 20 and <= 600 ? value : 60;
+    }
+
     public string Host { get; set; } = "127.0.0.1";
     public int Port { get; set; } = 5543;
     public bool DebugMode { get; set; }
